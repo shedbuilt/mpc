7cc5509f05c21517eb40bc41a106045ca7650639
Auston Stewart <auston@shedbuilt.net>
2018-01-22 10:27:06 -1000

    Updating to mpc version 1.1.0

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ./configure --prefix=/usr    \
             --disable-static \
-            --docdir=/usr/share/doc/mpc-1.0.3
-make -j $SHED_NUMJOBS
-make html
-make DESTDIR=${SHED_FAKEROOT} install
-make DESTDIR=${SHED_FAKEROOT} install-html
+            --docdir=/usr/share/doc/mpc-1.1.0 && \
+make -j $SHED_NUMJOBS && \
+make html && \
+make DESTDIR="$SHED_FAKEROOT" install && \
+make DESTDIR="$SHED_FAKEROOT" install-html
