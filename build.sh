f29ba55a016f464571ea43fb4a3ebe4e23db018f
Auston Stewart <auston@shedbuilt.net>
2018-05-29 19:30:04 -1000

    Adding package options

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+# Configure
+SHED_PKG_LOCAL_DOCDIR="/usr/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VESION}"
 ./configure --prefix=/usr    \
             --disable-static \
-            --docdir=/usr/share/doc/mpc-1.1.0 && \
-make -j $SHED_NUM_JOBS && \
-make html && \
-make DESTDIR="$SHED_FAKE_ROOT" install && \
-make DESTDIR="$SHED_FAKE_ROOT" install-html
+            --docdir=${SHED_PKG_LOCAL_DOCDIR} &&
+# Build and Install
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
+# Install Documentation
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
+    make html &&
+    make DESTDIR="$SHED_FAKE_ROOT" install-html || exit 1
+else
+    rm -rf "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_DOCDIR}"
+fi
